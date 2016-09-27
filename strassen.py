dd013adc2c7612e58de9200302afa784424c73ac
olgalupuleac <olga_lupuleac@mail.ru>
2016-09-27 19:45:32 +0300

    New version: function renamed, input/output changed

--- strassen.py
+++ strassen.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 
-def doublesplit(a):
+def split_in_quarters(a):
     return np.hsplit(np.vsplit(a, 2)[0], 2) + np.hsplit(np.vsplit(a, 2)[1], 2)
 
 
@@ -10,8 +10,8 @@ def strassen(a, b):
     if np.size(a) == 1:
         return np.dot(a, b)
     else:
-        a11, a12, a21, a22 = doublesplit(a)
-        b11, b12, b21, b22 = doublesplit(b)
+        a11, a12, a21, a22 = split_in_quarters(a)
+        b11, b12, b21, b22 = split_in_quarters(b)
         p1 = strassen(a11 + a22, b11 + b22)
         p2 = strassen(a21 + a22, b11)
         p3 = strassen(a11, b12 - b22)
@@ -23,17 +23,20 @@ def strassen(a, b):
         c12 = p3 + p5
         c21 = p2 + p4
         c22 = p1 - p2 + p3 + p6
-        return np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+        return np.vstack((np.hstack((c11, c12)),
+                          np.hstack((c21, c22))))
 
 
 n = int(input())
 k = 1
 while k < n:
-    k = k*2
+    k *= 2
 a = np.zeros((k, k), dtype=np.int)
 b = np.zeros((k, k), dtype=np.int)
-a[:n, :n] = np.loadtxt(sys.stdin, dtype=int)
-b[:n, :n] = np.loadtxt(sys.stdin, dtype=int)
-d = strassen(a, b)[:n, :n]
 for i in range(n):
-    print(*[d[i, j] for j in range(n)])
+    a[i, :] = list(map(int, input().split(' ')))
+for i in range(n):
+    b[i, :] = list(map(int, input().split(' ')))
+d = strassen(a, b)[:n, :n]
+for row in d:
+    print(' '.join(map(str, row)))
